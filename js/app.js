3cd66627f96499de682d35382f6934d2e91373a0
Paul <unkindest89@gmail.com>
2014-02-18 21:12:59 +0400

    Service Weather

--- js/app.js
+++ js/app.js
@@ -3,5 +3,55 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
+angular.module('myApp', [])
+        .provider('Weather', function() {
+            var apiKey = "";
+            this.setApiKey = function(key) {
+                if (key)
+                    this.apiKey = key;
+            };
+            this.$get = function($q,$http) {
+                var self = this;
+                return {
+                    getWeatherForecast: function(city){
+                        var d = $q.defer();
+                        $http({
+                            method:'GET',
+                            url: self.getUrl("forecast", city ),
+                            cache: true
+                        }).success(function(data){
+                            //wgd return object
+                            d.resolve(data.forecast.simpleforecast);
+                        }).error(function(err){
+                            d.reject(err);
+                        });
+                        return d.promise;
+                    }
+                    //Service object
+                }
+            }
+            this.getUrl = function(type, ext) {
+                return "http://api.wunderground.com/api/" +
+                        this.apiKey + "/" + type + "/q/" +
+                        ext + '.json';
+            }
+        })
+        .config(function(WeatherProvider) {
+            WeatherProvider.setApiKey('82921fa5ebe839db');
+        })
+        .controller('MainCtrl', function($scope, $timeout, Weather) {
+            //Build the date object
+            $scope.date = {};
+            //Update function
+            var updateTime = function() {
+                $scope.date.raw = new Date();
+                $timeout(updateTime, 1000)
+            }
+            //Kick off the update function
+            updateTime();
+            $scope.weather = {};
+            Weather.getWeatherForecast("CA/San_Francisco")
+                    .then(function(data){
+                        $scope.weather.forecast =data;
+                    });
+        });
